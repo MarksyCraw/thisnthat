090c276fe9e0a1972c0160403f9e14246fbef818
Mmark Mongeau <mark.mongeau@cra-arc.gc.ca>
2019-10-04 16:20:38 +0000

    second commit

--- DLT_provision.sh
+++ DLT_provision.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
  
-echo "hello World"
-#sudo apt-get update 
-#sudo apt-get install ubuntu-gnome-desktop -y
-#sudo apt-get install -y xrdp      
-#sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
+sudo apt-get update 
+sudo apt-get install ubuntu-gnome-desktop -y
+sudo apt-get install -y xrdp      
+sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
 
-#sudo apt-get install gnome-tweak-tool -y
+sudo apt-get install gnome-tweak-tool -y
