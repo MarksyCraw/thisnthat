c00df1e09b0cf169389370f77cf390fb31773c5f
Mmark Mongeau <mark.mongeau@cra-arc.gc.ca>
2019-10-04 18:28:21 +0000

    re-add sudo

--- DLT_provision.sh
+++ DLT_provision.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
  
-apt-get update 
-apt-get install ubuntu-gnome-desktop -y
-#sudo apt-get install -y xrdp      
-#sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
-
+sudo apt-get update 
+sudo apt-get install ubuntu-gnome-desktop -y
+sudo apt-get install -y xrdp      
+sudo sed -i 's/allowed_users=console/allowed_users=anybody/' /etc/X11/Xwrapper.config
 sudo apt-get install gnome-tweak-tool -y
